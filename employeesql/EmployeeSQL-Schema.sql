ae846ebf393b37aebafc7f57a4057db282afb804
MikeC <cruz.michael.j@gmail.com>
2020-01-03 19:08:13 -0500

    Version 1.0

---
+++ employeesql/EmployeeSQL-Schema.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+
+
+CREATE TABLE employees (
+  emp_no INT PRIMARY KEY,
+  birth_date DATE,
+  first_name VARCHAR(25),
+  last_name VARCHAR(25),
+  gender VARCHAR(1),
+  hire_date DATE
+);
+
+CREATE TABLE departments (
+  dept_no VARCHAR(4) PRIMARY KEY,
+  dept_name VARCHAR(25)
+);
+
+CREATE TABLE dept_emp (
+  emp_no INT,
+  dept_no VARCHAR(4),
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(4),
+  emp_no INT,
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT,
+  salary INT,
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT,
+  title VARCHAR(25),
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
